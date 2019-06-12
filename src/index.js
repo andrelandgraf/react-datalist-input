a28f70572b85d8ea22a6ebf8b45258063adc1691
andrelandgraf <andre.timo.landgraf@gmail.com>
2019-06-13 00:08:15 +0200

    fixing bugs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import './index.css';
+import './DataListInput.css';
 
 class DataListInput extends React.Component {
     constructor( props ) {
