9fa2ad5097597aafd0da3702204bd85bee5d6f7f
andrelandgraf <andre.timo.landgraf@gmail.com>
2019-08-14 15:37:25 +0200

    fixing bug with keys of type number on click with mouse

--- testing/demo-app/src/App.js
+++ testing/demo-app/src/App.js
@@ -1,49 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react';
 
-import logo from './logo.svg';
 import './App.css';
-
 import DataListInput from './DataListInput';
 
 const data = [
-  {
-    key: 0,
-    label: 'Apple',
-  },
-  {
-    key: 1,
-    label: 'Mango',
-  },
-  {
-    key: 2,
-    label: 'Potatoe',
-  },
-]
+    {
+        key: '0',
+        label: 'Apple',
+    },
+    {
+        key: '1',
+        label: 'Mango',
+    },
+    {
+        key: '2',
+        label: 'Potatoe',
+    },
+];
 
 function App() {
-  const [ item, setItem ] = useState();
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-        {
-          item && (
-          <div>
-            Current Item: { item.label }
-          </div>
-          )
-        }
-
-        <DataListInput
-          items={data}
-          onSelect={ ( i ) => setItem( i )}
-        />
-      </div>
-      </header>
-    </div>
-  );
+    const [ item, setItem ] = useState();
+    return (
+        <div className="App">
+            <div className="content">
+                {
+                    item && (
+                        <div>
+                            { `Current Item: ${ item.label }` }
+                        </div>
+                    )
+                }
+                <div className="wrapper">
+                    <DataListInput
+                        items={data}
+                        onSelect={i => setItem( i )}
+                    />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
